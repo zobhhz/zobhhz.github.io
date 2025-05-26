498a7339ac44d470b14e2e22dff3a3a699b7d9e9
Alyssa Palmares <alyssapalmares@yahoo.com>
2025-05-26 21:12:43 +0800

    added google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Analytics } from '@vercel/analytics/next';
-import Header from "@/components/Header";
-import type { Metadata } from "next";
 import { Atkinson_Hyperlegible } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from "next";
+import Script from 'next/script';
+import Header from "@/components/Header";
 import Footer from '@/components/Footer';
+import "./globals.css";
 
 const atkinsonHyperlegible = Atkinson_Hyperlegible({
   weight: ['400', '700'], // Regular (400) and Bold (700)
@@ -39,6 +40,17 @@ export default function RootLayout({
         <Footer />
         <Analytics />
         <SpeedInsights />
+        {/* Google tag (gtag.js) */}
+        <Script id="next" async src='https://www.googletagmanager.com/gtag/js?id=G-66GNBE41L3' />
+        <Script id="next">
+          {
+            `window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-66GNBE41L3');`
+          }
+        </Script>
       </body>
     </html>
   );
